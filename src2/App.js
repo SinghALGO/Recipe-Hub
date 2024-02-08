a7eacba3e789cc158598d4138270c529d818d39a
brittanyshelley <brittany.j.shelley@gmail.com>
2024-02-08 12:56:02 -0700

    Delete unnecessary files and code

---
+++ src2/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import TopNavbar from './components/TopNavBar';
+import RecipeCard from './components/RecipeCard';
+
+function App() {
+  return (
+    <div className="App">
+      <TopNavbar />
+      <RecipeCard />
+    </div>
+  );
+}
+
+export default App;
