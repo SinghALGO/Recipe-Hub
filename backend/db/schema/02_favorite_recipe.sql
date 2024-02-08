446cc6c565eb090d074a7faff2383e9e99a760bd
Yusuf Mahjoub <y.mahjoub1@outlook.com>
2024-02-08 15:52:16 -0500

    created databse and added seeds

--- backend/db/schema/02_favorite_recipe.sql
+++ backend/db/schema/02_favorite_recipe.sql
@@ -1,8 +1,8 @@
 DROP TABLE IF EXISTS favorite_recipe CASCADE;
 CREATE TABLE favorite_recipe (
-    favorite_recipe_id INT AUTO_INCREMENT PRIMARY KEY,
+    favorite_recipe_id SERIAL PRIMARY KEY,
     recipe_id INT,
     user_id INT,
     FOREIGN KEY (recipe_id) REFERENCES recipe(recipe_id),
-    FOREIGN KEY (user_id) REFERENCES user(user_id)
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
 );
