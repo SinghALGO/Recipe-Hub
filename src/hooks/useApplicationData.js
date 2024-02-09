f60690721875a6d78633891536b84250e43c8e12
SinghALGO <karanssandhu92@gmail.com>
2024-02-09 13:08:07 -0800

    added changes

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -65,7 +65,6 @@ const useApplicationData = () => {
 
   const getRecipes = async () => {
     try {
-      console.log(process.env.REACT_APP_API_KEY);
       const api = await fetch(
         `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=12`
       );
